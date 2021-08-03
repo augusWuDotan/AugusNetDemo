eab5b00be1914c1b9436521065a3d80281d52851
augus <augus-0331@hyper-light.net>
2021-08-03 13:37:22 +0800

    init

---
+++ AugusTestDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AugusTestDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
